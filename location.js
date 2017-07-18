c7a27f727b702f977bec4d6888dce3465b653f5d
coderman1738 <conorfitz1123@gmail.com>
2017-07-18 14:02:17 -0500

    Add files via upload

---
+++ location.js
@@ -0,0 +1,21 @@
+
+var x = document.getElementById("location_output")
+
+x.innerHTML(navigator.geolocation)
+
+
+
+function getLocation() {
+   if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+
